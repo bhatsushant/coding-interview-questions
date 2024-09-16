116a43af2e674f51468d84417b4f7e1af3aab2bc
Sushant Bhat <sush1393@gmail.com>
2024-09-16 11:17:46 -0400

    string reversal

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,43 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/* 1. Using a traditional for loop */
+/* 
+function reverse(str) {
+  let revstr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    revstr += str[i];
+  }
+  return revstr;
+}
+ */
+
+/* 2. Using the built-in reverse function */
+/* 
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
+ */
+
+/* 3. Using the for-of loop */
+/* 
+function reverse(str) {
+  let rev = "";
+  for (char of str) {
+    rev = char + rev;
+  }
+  return rev;
+}
+ */
+
+/* Using the reduce function */
+
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
 
 module.exports = reverse;
