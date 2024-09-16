c6e23a41ad02f8c9951cf5e64f8a717fc9b85c39
Sushant Bhat <sush1393@gmail.com>
2024-09-16 11:36:35 -0400

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,41 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+/* 1. Using the for-of loop */
+/* 
+function palindrome(str) {
+  let rev = "";
+  for (char of str) {
+    rev = char + rev;
+  }
+  return rev === str;
+}
+ */
+
+/* 2. Using the built-in reverse function */
+/* 
+function palindrome(str) {
+  const rev = str.split("").reverse().join("");
+  return rev === str;
+}
+ */
+
+/* 3. Using a traditional for loop */
+/* 
+function palindrome(str) {
+  let rev = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    rev += str[i];
+  }
+  return rev === str;
+}
+ */
+
+/* 4. Using the reduce function */
+
+function palindrome(str) {
+  const rev = str.split("").reduce((rev, char) => char + rev, "");
+  return rev === str;
+}
 
 module.exports = palindrome;
