6528b13ff2e04dfb88d6d8dc89ca349cf106dc0b
Sushant Bhat <sush1393@gmail.com>
2024-09-17 10:12:53 -0400

    maximum characters in a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,45 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+/* 1. Using traditional for and if loops */
+/* 
+ function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+    
+  for (char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[max];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+} 
+ */
+
+/* 2. Using the reduce function */
+
+function maxChar(str) {
+  const charMap = {};
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  return Object.keys(charMap).reduce((a, b) =>
+    charMap[a] > charMap[b] ? a : b
+  );
+}
 
 module.exports = maxChar;
