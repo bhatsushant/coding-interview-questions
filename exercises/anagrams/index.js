0c10506394bc46b258c685ca5627328ec5287390
Sushant Bhat <sush1393@gmail.com>
2024-10-01 09:25:33 -0400

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/* 1. Using hash maps and for loops */
+
+function anagrams(stringA, stringB) {
+  const charMapA = buildCharMap(stringA);
+  const charMapB = buildCharMap(stringB);
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+
+  for (let char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const buildCharMap = str => {
+  const charMap = {};
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+};
+
+/* 2. Using the built-in sort function */
+
+/* function anagrams(stringA, stringB) {
+  const cleanStr = str =>
+    str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+  return cleanStr(stringA) === cleanStr(stringB);
+} */
 
 module.exports = anagrams;
